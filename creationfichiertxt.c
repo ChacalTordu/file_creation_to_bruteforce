da5f0d850dc84d37fc1722afada9adc9a61464cf
ChacalTordu <leo.lebrun04@gmail.com>
2022-10-22 15:41:55 +0200

    Fonctions created to sweep and write in the file all the possibilites from a string given - It doesn't scan if there same possibilities already wrote in the file

--- creationfichiertxt.c
+++ creationfichiertxt.c
@@ -1,16 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
+#include <stdbool.h>
 #define SIZE_PSSWD 5
 #define POSSIBLE_CHARA 25
 #define NB_POSS SIZE_PSSWD^POSSIBLE_CHARA
+
+FILE *file_to_write;
+
+void make_combinations(char * str, char * tmp, int len, int ind) {
+  int i;
+   
+  if (ind >= len) {
+    //puts(tmp);
+    fprintf(file_to_write,"%s\n",tmp);
+    return;
+  }
+   
+  for (i = 0; i < len; i++) {
+    if (str[i] > 0) {
+      char pt = str[i];
+      str[i] = -1;
+      tmp[ind] = pt;
+      make_combinations(str, tmp, len, ind+1);
+      str[i] = pt;
+    }
+  }
+}
+ 
+void combinations(char * str) {
+  int len = strlen(str);
+  char * tmp = malloc(len + 1);
+   
+  tmp[len] = '\0';
+  make_combinations(str, tmp, len, 0);
+  free(tmp);
+}
 
 int main() {    
     printf("\nProgram for the creation of the current file ...\n\n---------------------------\n");
 
     //VARIABLE DECLARATION
-    FILE *file_to_write;
+    
     //char password_possibility[NB_POSS];
     char alphabet[26];
     int i,j;
@@ -47,7 +78,7 @@ int main() {
 
     if (file_to_write != NULL)
     {
-        //WRITING
+        //WRITING POSSIBILITES W/ SAME CHARACTER
         for (j = 0; j < 26; j++)
         {
             for (i = 0; i < 6; i++)
@@ -55,13 +86,16 @@ int main() {
                 string_to_print[i]=alphabet[j];
             }
             string_to_print[6]='\0';
-            printf("After concatenation = %s\n", string_to_print);
+            //printf("%s\n", string_to_print);
             fprintf(file_to_write,"%s\n",string_to_print);
         }
+
+        char str[] = "abcdef";
+        combinations(str);
     }
     else
-        printf("file not opened");
-
+        printf("ERROR : file not opened");
     fclose(file_to_write);
+    printf("\n______________FILE CREATED______________\n");
     return 0;
 }
